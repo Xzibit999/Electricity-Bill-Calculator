3f3be1a88a4e76d38a0b9ed7c7385dc1d3fdea0e
Xzibit999 <magarsujal123@gmail.com>
2025-03-09 05:27:56 +0000

    adding electricity_bill program

--- electricity_bill.c
+++ electricity_bill.c
@@ -1,1 +1,34 @@
 #Code for calculating electricity bill
+#include <stdio.h>
+
+// Function to calculate the bill
+float calculateBill(int units) {
+    float bill = 0;
+
+    if (units <= 100) {
+        bill = units * 1.5;
+    } else if (units <= 300) {
+        bill = (100 * 1.5) + (units - 100) * 2.5;
+    } else {
+        bill = (100 * 1.5) + (200 * 2.5) + (units - 300) * 3.5;
+    }
+
+    return bill;
+}
+
+int main() {
+    int units;
+    float bill;
+
+    // Get the number of units consumed from the user
+    printf("Enter the number of units consumed: ");
+    scanf("%d", &units);
+
+    // Calculate the bill
+    bill = calculateBill(units);
+
+    // Print the bill
+    printf("Total electricity bill is: %.2f\n", bill);
+
+    return 0;
+}
